6b2e36d9ef729caf9e6f797c53580ae73e242918
cuzcode <64682384+cuzcode@users.noreply.github.com>
2020-05-02 10:19:50 +0100

    index file adjusted

--- index.php
+++ index.php
@@ -3,5 +3,7 @@
 <head><title>Trying Hitub</title></head>
 <body>
 <script>alert("hello gitHub")</script>
+  <h2>Just Trying To Learn GitHub</h2>
+  <p>My Main Nigger, programmer Sam said this is a hard stuff</p>
 </body>
 </html>
